0de49b3a0f425fee320ee81451f908a8fff280df
Linn Ammouri <108581680+l1nn1l@users.noreply.github.com>
2025-09-26 21:16:51 +0200

    implemented Keyed LSB pdf watermarking method

--- server/src/watermarking_utils.py
+++ server/src/watermarking_utils.py
@@ -43,6 +43,7 @@ from watermarking_method import (
 )
 from add_after_eof import AddAfterEOF
 from unsafe_bash_bridge_append_eof import UnsafeBashBridgeAppendEOF
+from watermarking_lsb import LSBWatermark
 
 # --------------------
 # Method registry
@@ -50,7 +51,8 @@ from unsafe_bash_bridge_append_eof import UnsafeBashBridgeAppendEOF
 
 METHODS: Dict[str, WatermarkingMethod] = {
     AddAfterEOF.name: AddAfterEOF(),
-    UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF()
+    UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF(),
+    LSBWatermark.name: LSBWatermark(), #Linn
 }
 """Registry of available watermarking methods.
 
